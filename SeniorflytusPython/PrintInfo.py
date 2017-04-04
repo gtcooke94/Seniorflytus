e7a24418d78eddf48c12bcf90fe117bd579f36fa
Gregory Cooke <gtcooke94@gmail.com>
2017-04-04 14:00:29 -0400

    Added PrintInfo.py file for data gathering and several log files

--- SeniorflytusPython/PrintInfo.py
+++ SeniorflytusPython/PrintInfo.py
@@ -2,7 +2,9 @@
 import time, sys
 import ps_drone # Import PS-Drone-API
 
-drone = ps_drone.Drone() # Start using drone	
+fileName = raw_input("Log File Name: " )
+fileName = fileName + ".txt"
+drone = ps_drone.Drone() # Start using drone    
 drone.startup() # Connects to drone and starts subprocesses
 
 drone.reset() # Sets drone's status to good
@@ -10,8 +12,11 @@ while (drone.getBattery()[0]==-1): time.sleep(0.1) # Wait until drone has done i
 print "Battery: "+str(drone.getBattery()[0])+"% "+str(drone.getBattery()[1]) # Battery-status
 if drone.getBattery()[1]=="empty": sys.exit() # Give it up if battery is empty
 
-drone.useDemoMode(True) # Set 15 basic dataset/sec (default anyway)
-drone.getNDpackage(["demo"]) # Packets, which shall be decoded
+drone.useDemoMode(False) # Set 15 basic dataset/sec (default anyway)
+drone.addNDpackage(["demo"]) # Packets, which shall be decoded
+drone.addNDpackage(["magneto"])
+drone.addNDpackage(["time"])
+drone.getNDpackage(["all"])
 time.sleep(0.5) # Give it some time to awake fully
 
 #drone.trim() # Recalibrate sensors
@@ -25,21 +30,41 @@ while drone.NavData["demo"][0][2]: time.sleep(0.1) # Wait until drone is complet
 print "Drone is flying now"
 
 stop = False
+
+
+# From turnAngle()
+navData = drone.NavData
+#opos = navData["demo"][2][2]            # get the source/current (original) angle                                 # ...be correctly handled
+speed = drone.speed
+ospeed = speed                                   # stores the given speed-value
+reftime = time.time()
+accurateness = 0
+f1 = open(fileName, 'w')  
+# End from turnAngle   
+#drone.getSelfRotation()
 while (not stop):
     if (drone.getKey() == 'c'): stop = True
     drone.hover()
-		while not stop and counter<=5:
-		ndc = self.__NavDataCount						# wait for the next NavData-package
-		while ndc == self.__NavDataCount:		time.sleep(0.001)
-		kalib = (time.time()-reftime)*self.selfRotation	# trys to recalibrate, causing moving sensor-values around 0.0185 deg/sec
-		cpos = self.__NavData["demo"][2][2]				# get the current angle
-		if minaxis > cpos:			minaxis = cpos		# set the minimal seen angle
-		if maxaxis < cpos:			maxaxis = cpos		# set the maximal seen angle
-		if cpos-minaxis >= 180:		cpos = cpos-360		# correct the angle-value if necessary...
-		elif maxaxis-cpos >= 180:	cpos = cpos+360		# ...for an easier calculation
-		speed = abs(cpos-npos+kalib) / 10.0				# the closer to the destination the slower the drone turns
+    ndc = drone.NavDataCount                      # wait for the next NavData-package
+    while ndc == drone.NavDataCount:
+        time.sleep(0.0001)
 
-    
+    kalib = (time.time()-reftime)*drone.selfRotation # trys to recalibrate, causing moving sensor-values around 0.0185 deg/sec
+    navData = drone.NavData
+    cpos = navData["demo"][2][2]             # get the current angle
+    vx = navData["demo"][4][0]
+    vy = navData["demo"][4][1]
+    vz = navData["demo"][4][2]
+    mx = navData["magneto"][0][0]
+    my = navData["magneto"][0][1]
+    mz = navData["magneto"][0][2]
+    times = navData["time"][0]
+    #print "cpos = " + str(cpos)
+    #print >> f1, "vx, vy, vz = " + str(vx) + ", " + str(vy) + ", " + str(vz)
+    #print >> f1, "mx, my, mz = " + str(mx) + ", " + str(my) + ", " + str(mz) + "\n"
+    #print "mx, my, mz = " + str(mx) + ", " + str(my) + ", " + str(mz) + "\n"
+    #print "kalib = " + str(kalib)
+    print >> f1, str(mx) + ", " + str(my) + ", " + str(mz) + ", " + str(times)
 
 drone.stop()
 time.sleep(2)
